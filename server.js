9dab6651433992b6d2361570609b5234d813e0b3
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-07-07 20:55:39 +0200

    feat: serve static files from the public directory

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.API_SERVER_PORT;
 const connection = require("./database/connection");
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
