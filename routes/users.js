b0f3921d899df27a9a9be5342ed5ba1e91c5a889
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-07-08 23:44:40 +0200

    feat: add user routes for fetching all users and a single user by ID

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+
+// Get route [Index] to fetch all users from the database
+router.get("/",);
+
+
+
+// Get route [Show] to fetch a single user by ID from the database
+router.get("/:id",);
+
+module.exports = router;
