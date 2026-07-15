033bcbc1772d40a67503a29758904d279db9d3f7
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-07-15 17:51:36 +0200

    fix: correct frontend server port in .env.example and reorder cors import in server.js

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.API_SERVER_PORT;
 const moviesRouter = require("./routes/movies");
 const notFound = require("./middlewares/notFound");
 const serverError = require("./middlewares/serverError");
-const cors = require("cors");
+
 
 app.use(cors({
     origin: process.env.FRONTEND_SERVER_PORT
 }));
 app.use(express.static("public"));
+app.use(express.json());
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
@@ -28,3 +27,6 @@ app.use(notFound);
 // catch-all route for 404 errors
 app.use(serverError);
 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
